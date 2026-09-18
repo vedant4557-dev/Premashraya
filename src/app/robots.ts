dba2a112344c271e65adb37b2ccec07ffcc9b5a9
vedant4557-dev <vedant4557@gmail.com>
2026-09-18 10:21:39 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ["/api/", "/_next/"],
       },
     ],
-    sitemap: "https://premashraya.org/sitemap.xml",
+    sitemap: "https://premashraya.in/sitemap.xml",
   };
 }
