62bd9490918df604648ed5697ad81c907414fbff
vedant4557-dev <vedant4557@gmail.com>
2026-09-18 10:20:48 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://premashraya.org";
+  const baseUrl = "https://premashraya.in";
   const lastModified = new Date();
 
   return [
